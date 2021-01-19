bc3991ac62a93c12ffb540baefc5de0cd9a10b29
tananun <tananun_c@cmu.ac.th>
2021-01-19 18:11:48 +0700

    F

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,22 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0,sq = 0,sumRe=0,GM=1;
+    double Max = A[0] ,Min = A[0];
+    for (int i = 0; i < N; i++){
+        sum = sum + A[i];
+        sq += pow(A[i],2.0);
+        GM = GM*A[i];
+        sumRe = sumRe + 1/A[i]; 
+        if (A[i]>Max) Max = A[i];
+        if(A[i]<Min) Min = A[i];
+    }
+    B[0] = sum/N;
+    B[1] = pow((sq/N-B[0]*B[0]),0.5);
+    B[2] = pow(GM,(1.0/N));
+    B[3] = N/sumRe; 
+    B[4] = Max;
+    B[5] = Min;
+}
